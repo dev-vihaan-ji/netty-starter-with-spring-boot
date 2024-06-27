41a926aa64c4857ac4878b7ed5945d27b845c055
dev-vihaan-ji <dev.vihaan.ji@gmail.com>
2024-06-27 23:20:46 +0900

    first commit 2024-06-27 vihann ji

---
+++ src/test/java/com/dev/vihaan/netty/starter/with/spring_boot/NettyStarterWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.vihaan.netty.starter.with.spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyStarterWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
